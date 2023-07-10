67542cf7ed60b7e527baf38d87689a3d0c4f51dc
oscar <oscar@flashphoner.com>
2023-07-10 13:29:25 +0700

    Actual FPWebRTC build 2.6.111 #WCS-3862

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWebRTC-2.6.111.zip",
+            checksum: "dd5ddafa88227493c0416f665aae701a932ec3227fd6dfa5003dbd0804f05f62"
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Flashphoner WebCallServer WebRTC library
+
+Flashphoner WebCallServer WebRTC library patched to use with iOS SDK to build native iOS WebRTC streaming applications.
+
+Please see detailed description [here](https://docs.flashphoner.com/display/IOSSDK2EN/Overview)
