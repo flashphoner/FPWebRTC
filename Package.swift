46c4abd348030c8cf9c4ea4a7a9c5c54d6232a10
Flashphoner <support@flashphoner.com>
2025-10-07 10:09:18 +0600

    Version 2.6.133

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWebRTC-2.6.131.zip",
-            checksum: "09fbf61043627e1492569804a3316f983a3e7f14a6c48e3f7cd4bbe7a564b1c3"
+            url: "https://flashphoner.com/downloads/builds/flashphoner_client/wcs-ios-sdk/pods/FPWebRTC-2.6.133.zip",
+            checksum: "4b0e4eb6638b429741ca61c048ba223aae9e21394e0bf469e91a0d2a41106109"
         )
     ]
 )
